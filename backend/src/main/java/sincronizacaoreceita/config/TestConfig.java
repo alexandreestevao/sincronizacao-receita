da332b9daf9fd84b00ecdb55661de17219616a75
Alexandre Estevao <alexandresestevao@gmail.com>
2020-10-02 15:43:26 -0300

    Atualização do arquivo TestConfig.java

--- backend/src/main/java/sincronizacaoreceita/config/TestConfig.java
+++ backend/src/main/java/sincronizacaoreceita/config/TestConfig.java
@@ -18,71 +18,69 @@ import sincronizacaoreceita.services.ReceitaService;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Started...");
 		System.out.println("");
 		reader();
 		System.out.println("");
-		System.out.println("End of processing!");		
+		System.out.println("End of processing!");
 	}
-	
-	// Leitura do arquivo
-		public void reader() {
 
-			//String path = "c:\\temp\\in.csv";
-			String path = "source\\in.csv";
+	// Leitura do arquivo de entrada "in.csv' no diretório "source"
+	public void reader() {
 
-			List<Account> list = new ArrayList<Account>();
+		String path = "source\\in.csv";
 
-			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		List<Account> list = new ArrayList<Account>();
 
-				String line = br.readLine();
-				line = br.readLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-				while (line != null) {
-					String[] vect = line.split(";");
-					String agencia = vect[0];
-					String conta = vect[1].replace("-", "");
-					Double saldo = Double.parseDouble(vect[2].replace(",", "."));
-					String status = vect[3];
+			String line = br.readLine();
+			line = br.readLine();
 
-					ReceitaService receitaService = new ReceitaService();
-					try {
-						receitaService.atualizarConta(agencia, conta, saldo, status);
-					} catch (RuntimeException e) {
-						e.printStackTrace();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+			while (line != null) {
+				String[] vect = line.split(";");
+				String agencia = vect[0];
+				String conta = vect[1].replace("-", "");
+				Double saldo = Double.parseDouble(vect[2].replace(",", "."));
+				String status = vect[3];
 
-					Account account = new Account(agencia, conta, saldo, status);
-					list.add(account);		
-					
-					System.out.println(line);
-					writer(line);
-					line = br.readLine();
-					
+				ReceitaService receitaService = new ReceitaService();
+				try {
+					receitaService.atualizarConta(agencia, conta, saldo, status);
+				} catch (RuntimeException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 
-			} catch (IOException e) {
-				System.out.println("Error: " + e.getMessage());
+				Account account = new Account(agencia, conta, saldo, status);
+				list.add(account);
+
+				System.out.println(line);
+				writer(line);
+				line = br.readLine();
+
 			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
+	}
 
-		// Escrita do arquivo .CSV
-		public void writer(String line) {
+	// Escrita do arquivo de saída "out.csv" no diretório destiny 
+	public void writer(String line) {
 
-			//String path = "c:\\temp\\out.csv";			
-			String path = "destiny\\out.csv";
+		String path = "destiny\\out.csv";
 
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-				bw.write(line);
-				bw.newLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}	
-	
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			bw.write(line);
+			bw.newLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
