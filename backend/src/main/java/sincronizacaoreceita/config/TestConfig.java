0b4fc3f5485bf445d9d2ef4f06458ea76eba9413
Alexandre Estevao <alexandresestevao@gmail.com>
2020-10-02 15:27:17 -0300

    Projeto de Leitura e Escrita de Arquivo

---
+++ backend/src/main/java/sincronizacaoreceita/config/TestConfig.java
@@ -0,0 +1,88 @@
+package sincronizacaoreceita.config;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import sincronizacaoreceita.entities.Account;
+import sincronizacaoreceita.services.ReceitaService;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Started...");
+		System.out.println("");
+		reader();
+		System.out.println("");
+		System.out.println("End of processing!");		
+	}
+	
+	// Leitura do arquivo
+		public void reader() {
+
+			//String path = "c:\\temp\\in.txt";
+			String path = "source\\in.txt";
+
+			List<Account> list = new ArrayList<Account>();
+
+			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+				String line = br.readLine();
+				line = br.readLine();
+
+				while (line != null) {
+					String[] vect = line.split(";");
+					String agencia = vect[0];
+					String conta = vect[1].replace("-", "");
+					Double saldo = Double.parseDouble(vect[2].replace(",", "."));
+					String status = vect[3];
+
+					ReceitaService receitaService = new ReceitaService();
+					try {
+						receitaService.atualizarConta(agencia, conta, saldo, status);
+					} catch (RuntimeException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					Account account = new Account(agencia, conta, saldo, status);
+					list.add(account);		
+					
+					System.out.println(line);
+					writer(line);
+					line = br.readLine();
+					
+				}
+
+			} catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+
+		// Escrita do arquivo .CSV
+		public void writer(String line) {
+
+			//String path = "c:\\temp\\out.csv";			
+			String path = "destiny\\out.csv";
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+				bw.write(line);
+				bw.newLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
+	
+}
---
+++ backend/src/test/java/com/com/sincronizacaoreceita/SincronizacaoreceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.sincronizacaoreceita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SincronizacaoreceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
